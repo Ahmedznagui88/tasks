b29ae74ee0f8e1759a4e2cee3011611021719f9f
Ahmed Znagui <amed.bob88@gmail.com>
2024-11-15 01:02:51 +0100

    len str control

--- task5.cpp
+++ task5.cpp
@@ -6,16 +6,36 @@ using namespace std;
 #define MAX_LENGTH 100
 
 int main() {
-    char strA[MAX_LENGTH, '\0' ];
-    char strB[MAX_LENGTH, '\0' ];
+    char strA[MAX_LENGTH];
+    char strB[MAX_LENGTH];
     
-    int compare = strcmp(strA, strB);
     //! input validate if word have spaceword
     do {
         cout << "Enter first word: ";
-        cin.getline(strA, MAX_LENGTH, '\0' );
+        cin.getline(strA, MAX_LENGTH);
         cout << "Enter second word: ";
-        cin.getline(strB, MAX_LENGTH, '\0' );
+        cin.getline(strB, MAX_LENGTH);
+
+
+        //! check if the two words are anagrams
+
+        for (int i = 0; i < strlen(strA); i++) {
+            for (int j = 0; j < strlen(strA); j++) {
+
+                if (strA[i] == strA[j]) {
+                    i++;
+                }
+                if (strA[i] == strB[j]) {
+                    i++;
+                }
+            }
+        }
+
+        if (strlen(strA) != strlen(strB)) {
+            cout << "Thi is not an anagram" << endl;
+        } else {
+            cout << "this is an anagrma" << endl;
+        }
 
     } while (strchr(strA, ' ') && strchr(strB, ' '));
 
