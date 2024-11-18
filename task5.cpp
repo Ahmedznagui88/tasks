9b69ba93364ab5329ea67becc2352fb883bc3b69
Ahmed Znagui <amed.bob88@gmail.com>
2024-11-18 21:15:35 +0100

    improve remove space add an inner for controlling spacing

--- task5.cpp
+++ task5.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_LENGTH 100
+#define MAX_LENGTH 101
 int main() {
+
     char strA[MAX_LENGTH], strB[MAX_LENGTH];
     //! input: strings until press $
     do {
@@ -13,9 +14,19 @@ int main() {
             cin.getline(strA, MAX_LENGTH, '\n');
             if (failed){
                 cin.clear();
-                cin.ignore(100, '\n');
+                cin.ignore(101, '\n');
             }
+            //! remove spaces from string 1
+            for (int i = 0; i < strlen(strA); i++) {
+                if (strA[i] == ' ') {  
+                    for (int j = i; j < strlen(strA); j++){
+                        strA[j] = strA[j + 1];
+                    }
+                }
+            }
+
         } while (failed);
+
         //! press $ to exit
         if (strcmp(strA, "$") == 0) {
             cout << "Bye" << endl;
@@ -28,26 +39,23 @@ int main() {
             cin.getline(strB, MAX_LENGTH, '\n');
             if (failed){
                 cin.clear();
-                cin.ignore(100, '\n');
+                cin.ignore(101, '\n');
+            }
+            //! remove spaces from string 2 
+            for (int i = 0; i < strlen(strB); i++) {
+                if (strB[i] == ' ') {
+                    for (int j = i; j < strlen(strB); j++){
+                        strB[j] = strB[j + 1];
+                    }  
+                } 
             }
         } while (failed);
-
-        //! remove spaces from strings
-        for (int i = 0; i < strlen(strA); i++) {
-            if (strA[i] == ' ') {
-                strA[i] = strA[i - 1];
-            }
-            for (int j = 0; j < strlen(strB); j++) {
-                if (strB[j] == ' ') {
-                    strB[j] = strB[j - 1];
-                }
-            }
-        }
         cout << endl;
 
         //! compare strings
         //! check the first letters from the first and second string
         //! check if two strings are anagrams or not
+        
         for (int i = 0; i < strlen(strA); i++) {
             if (strA[0] == strB[0]) {
                 cout << " ";
@@ -59,7 +67,7 @@ int main() {
             }
             break;
         }
-        cout << endl;
+
     } while (strcmp(strA, "$") != 0);
     return 0;
 }
