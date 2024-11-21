a0f7eb2bcb34c32959997220fd6ebc0f46728cfc
Ahmed Znagui <amed.bob88@gmail.com>
2024-11-21 23:35:52 +0100

    new task 'Contacts'

--- task5.cpp
+++ task5.cpp
@@ -1,74 +1,54 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_LENGTH 101
+#define LEN 101
 int main() {
 
-    char strA[MAX_LENGTH], strB[MAX_LENGTH];
-    //! input: strings until press $
+    char strA[LEN], strB[LEN];
     do {
-        
-        //! First word / fail management
+    int failed = cin.fail();
+ 
         do {
-            cout << "Enter the first word: ";
-            cin.getline(strA, MAX_LENGTH, '\n');
-            if (cin.fail()){
+            cout << "Enter the first word: " <<endl;
+            cin.getline(strA, LEN, '\n');
+            if (failed){
                 cin.clear();
                 cin.ignore(101, '\n');
                 cout << "Invalid input, try again" << endl;
+                break;
             }
-        } while (!cin || strlen(strA) > MAX_LENGTH);
+        } while (strlen(strA) > 101); 
 
-        //! press $ to exit
         if (strcmp(strA, "$") == 0) {
             cout << "Bye" << endl;
             return 0;
         }
 
-        //! Second word / fail management
         do {
-            cout << "Enter the second word: ";
-            cin.getline(strB, MAX_LENGTH, '\n');
-            if (cin.fail()){
+            cout << "Enter the second word: " <<endl;
+            cin.getline(strB, LEN, '\n');
+            if (failed){
                 cin.clear();
                 cin.ignore(101, '\n');
                 cout << "Invalid input, try again" << endl;
             }
-        } while (!cin || strlen(strB) > MAX_LENGTH); 
+        } while (strlen(strB) > 101); 
 
-        //! remove spaces from string 1 & 2
-        for (int i = 0; i < strlen(strA); i++) {
-            if (strA[i] == ' ') {  
-                for (int j = i; j < strlen(strA); j++){
-                   strA[j] = strA[j + 1];
+        cout << endl;
+
+            for (int i = 0; i < strA[i]; i++) {
+                if (strA[i] == strB[i]) {
+                    break;
+                } else if (strchr(strB, strA[i]) == 0 || strlen(strA) != strlen(strB)) {
+                    cout << "The two words do not have similar letters" << endl;
+                    break;
+                } else {
+                    cout << "Words are anagrams" << endl;
+                    break;
                 }
             }
-        }
-        for (int i = 0; i < strlen(strB); i++) {
-            if (strB[i] == ' ') {
-                for (int j = i; j < strlen(strB); j++){
-                    strB[j] = strB[j + 1];
-                }  
-            } 
-        }
-
-        cout << endl;
-
-        //! check if the words are anagrams
-        for (int i = 0; i < strA[i]; i++) {
-            if (strA[i] == strB[i]) {
-                cout << " ";
-                break;
-            } else if (strchr(strB, strA[i]) == 0) {
-                cout << "This is not an anagram" << endl;
-                break;
-            } else {
-                cout << "Words are anagrams" << endl;
-                break;
-            }
-        }
-        cout << endl;
-
+            cout << endl;
+            
     } while (strcmp(strA, "$") != 0);
     return 0;
 }
---
+++ task6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct contacts {
+    char name[50];
+    char surname[50];
+    char phone[50];
+};
+
+int main() {
+    
+    
+    
+    return 0;
+}
