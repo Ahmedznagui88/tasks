3ac11c4dd961a2cab507672d317d1369424f8c87
Ahmed Znagui <amed.bob88@gmail.com>
2024-11-23 04:00:38 +0100

    improve input name and surname

--- task6.cpp
+++ task6.cpp
@@ -11,22 +11,23 @@ struct contacts {
 
 int main() {
 
+    contacts contact[LEN];
+    contacts c;
+    char name[50], surname[50], phone[10];
+    int count = 0;
     int choice;
-    cout << "1 - Show Contact" << endl;
-    cout << "2 - Insert contact" << endl;
-    cout << "3 - Delete Contact" << endl;
-    cout << "4 - Exit" << endl;
-
-    cout << "Enter a choice : ";
-    cin >>choice;
 
     do {
 
-        contacts contact[LEN];
-        contacts c;
-        string name, surname, phone;
-        int count = 0;
+        cout << "***** M E N U *****" << endl;
+        cout << "1 - Show Contact" << endl;
+        cout << "2 - Insert contact" << endl;
+        cout << "3 - Delete Contact" << endl;
+        cout << "4 - Exit" << endl;
 
+        cout << "Enter a choice : ";
+        cin >>choice;
+        
         switch (choice) {
         case 1:
             for (int i = 0; i < count; i++) {
@@ -37,30 +38,34 @@ int main() {
 
             break;
         case 2:
-        cout << "Inserisci contatto" << endl;
-            cout << "Enter a name : ";
-            cin.getline(c.name, 50);
-            do {
-                cout << "Enter a name : ";
-                cin.getline(c.name, 50);
-                if (cin.fail()) {
-                    cin.clear();
-                    cin.ignore(50, '\n');
-                }
-                cout << "Invalid input : " << endl;
-            } while (strlen(c.name) > 50 || strlen(c.name) > 50);
 
-                cout << "Enter a surname : ";
-                cin.getline(c.surname, 50);
-            do {
-                cout << "Invalid input : " << endl;
-                cout << "Enter a surname : ";
-                cin.getline(c.surname, 50);
+            cout << "Enter a name : " << endl;
+            cin.getline(c.name, 50);
+            
+            while(c.name[0] == '\0') {
                 if (cin.fail()) {
                     cin.clear();
                     cin.ignore(50, '\n');
+                    cout << "Invalid input : " << endl;
+                    cout << "Enter a name : " << endl;
                 }
-            } while (strlen(c.surname) > 50 || strlen(c.surname) > 50);
+            }
+            
+            cout << "Enter a surname : " << endl;
+            cin.getline(c.surname, 50);
+                
+            cout << "Enter a surname : " << endl;
+            cin.getline(c.surname, 50);
+            while(c.surname[0] == '\0') {
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(50, '\n');
+                    cout << "Invalid input : " << endl;
+                    cout << "Enter a surname : " << endl;
+                }
+            }
+
+            strcpy(contact[count].surname, c.surname);
 
             int n;
             cout << "Enter a phone number: ";
@@ -83,20 +88,37 @@ int main() {
                 }
 
             } while (n > 11 || n <= 1);
+
+            strcpy(contact[count].phone, c.phone);
             break;
         case 3:
-            cout << "Cancella contatto" << endl;
+            cout << "Delete Contact" << endl;
+            if(count == 0) {
+                cout << "No contact to delete" << endl;
+                break;
+            }else {
+                cout << "Delete contact" << endl;
+            }
+            
             break;
         case 4:
-            cout << "Esci" << endl;
+
+        if (choice == 4) {
+            cout << "Exit" << endl;
+            break;
+        }
 
             break;
         default:
             cout << "Invalid input" << endl;
             
         }
+        contact[count] = c;
+        count++;
+        
+        
+    } while (choice != 4);
 
-    } while (true);
 
     return 0;
 }
