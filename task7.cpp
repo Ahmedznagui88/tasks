8f2a8ca4caf50c87d536587403c214c3b0e0c9d7
Ahmed Znagui <amed.bob88@gmail.com>
2024-11-30 22:47:12 +0100

    task7

---
+++ task7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[1001];
+
+int insert(int x) {
+    arr[x] = x;
+    x++;
+    return x;
+}
+
+
+int main() {
+
+    
+    
+    
+    
+    return 0;
+}
