68e50e91e227d10a4748f442af3de94bdd016cab
Ahmed Znagui <amed.bob88@gmail.com>
2024-12-02 14:07:58 +0100

    update

--- task7.cpp
+++ task7.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 using namespace std;
 
+int *p = new int[1001];
 
-int arr[1001];
+int n = 0;
 
-int insert(int x) {
-    arr[x] = x;
-    x++;
-    return x;
+void add(int &x) {
+    p[n] = x;
+    n++;
 }
-
 
 int main() {
 
-    
-    
-    
+    int x;
+    do {
+        cout << "Enter a number : ";
+        cin >> x;
+        add(x);
+        if(cin.fail()) {
+            cin.clear();
+            cin.ignore(1001, '\n');
+            cout << "Invalid input, Please enter a valid number : ";
+        }
+    } while (x > -1);
+
+    for (int i = 0; i < n; i++) {
+        if (x == p[i]) {
+            for (int j = i; j < n - 1; j++) {
+                p[j] = p[j + 1];
+            }
+            n--;
+        }
+        cout << p[i] << " ";
+    }
+    delete [] p;
     
     return 0;
 }
